6748db9622066821d0d917cf1ed4d5fcc0dfed88
정인호 <derosatam76@gmail.com>
2020-12-11 00:47:59 +0900

    Create function to show on the web Create showResult(), cubeToString(), lineToString(), blankOutput(), makeDom(), initialCube() function

--- components/ImplementRubiksCube.js
+++ components/ImplementRubiksCube.js
@@ -1,45 +1,36 @@
 import consoleCube from '../helper/RubiksCubeConsoleOutput.js';
 
-// 입력받은 명령에 따라 루빅스 큐브 밀기 밀기
-function pushRubiksCube(value) {
-  console.log(value);
-  for (let i = 0; i < value.length; i++) {
-    if (value[i].toUpperCase() === 'Q') {
-      // deleteEvent();
-      return;
-    } else if (Number(value[i][1]) === 2) {
-      turnCube(value[i]);
-    }
-    turnCube(value[i]);
-    return;
-    // setTimeout(() => showResult(char), 500 * i);
+// 파일을 읽을 때 input 박스에 이벤트를 건다.
+function init() {
+  window.rubiksCube = {
+    up: makeOneSideCube('W'),
+    left: makeOneSideCube('G'),
+    front: makeOneSideCube('R'),
+    right: makeOneSideCube('B'),
+    back: makeOneSideCube('O'),
+    down: makeOneSideCube('Y'),
+  };
+
+  const inputValue = document.querySelector('#inputValue');
+  inputValue.addEventListener('keypress', pressEnter);
+  makeDom('#initialCube', 'div', 'cube', cubeToString());
+}
+
+// 입력 받은 색깔로 2차원 배열 생성
+function makeOneSideCube(color) {
+  return [
+    [color, color, color],
+    [color, color, color],
+    [color, color, color],
+  ];
+}
+
+// enter 클릭 시 입력값에 대한 유효성검사 함수 실행.
+function pressEnter(e) {
+  if (e.keyCode === 13) {
+    validateInputValue(e.target.value);
+    e.target.value = '';
   }
-}
-
-// 입력 받은 문자열 재조합
-function recombinantString(value) {
-  return value.split('').reduce((acc, cur) => {
-    if (cur === "'" || Number(cur)) {
-      acc[acc.length - 1] += cur;
-    } else {
-      acc.push(cur);
-    }
-    return acc;
-  }, []);
-}
-
-// 알파벳에 대한 유효성 검사
-function validateChar(char) {
-  char = char.toUpperCase();
-  return (
-    char === 'U' ||
-    char === 'L' ||
-    char === 'F' ||
-    char === 'R' ||
-    char === 'B' ||
-    char === 'D' ||
-    char === 'Q'
-  );
 }
 
 // 입력값에 대한 유효성 검사
@@ -65,60 +56,50 @@ function validateInputValue(value) {
   pushRubiksCube(recombinantString(value));
 }
 
-// enter 클릭 시 입력값에 대한 유효성검사 함수 실행.
-function pressEnter(e) {
-  if (e.keyCode === 13) {
-    validateInputValue(e.target.value);
-    e.target.value = '';
+// 알파벳에 대한 유효성 검사
+function validateChar(char) {
+  char = char.toUpperCase();
+  return (
+    char === 'U' ||
+    char === 'L' ||
+    char === 'F' ||
+    char === 'R' ||
+    char === 'B' ||
+    char === 'D' ||
+    char === 'Q'
+  );
+}
+
+// 입력 받은 문자열 재조합
+function recombinantString(value) {
+  return value.split('').reduce((acc, cur) => {
+    if (cur === "'" || Number(cur)) {
+      acc[acc.length - 1] += cur;
+    } else {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+}
+
+// 입력받은 명령에 따라 루빅스 큐브 밀기 밀기
+function pushRubiksCube(value) {
+  for (let i = 0; i < value.length; i++) {
+    if (value[i].toUpperCase() === 'Q') {
+      // deleteEvent();
+    } else if (Number(value[i][1]) === 2) {
+      turnCube(value[i]);
+    }
+
+    if (value[i].toUpperCase() !== 'Q') {
+      turnCube(value[i]);
+    }
+    showResult(value[i].toUpperCase());
+    // setTimeout(() => showResult(char), 500 * i);
   }
 }
 
-function makeOneSideCube(color) {
-  return [
-    [color, color, color],
-    [color, color, color],
-    [color, color, color],
-  ];
-
-  // [
-  //   ['a', 'b', 'c'],
-  //   ['a', 'b', 'c'],
-  //   ['a', 'b', 'c'],
-  // ];
-  // [
-  //   ['a', 'a', 'a'],
-  //   ['b', 'b', 'b'],
-  //   ['c', 'c', 'c'],
-  // ];
-  //   [
-  //     ['a', 'b', 'c'],
-  //     ['d', 'e', 'f'],
-  //     ['g', 'h', 'i'],
-  //   ];
-  //   [
-  //     [color, color, color],
-  //     [color, color, color],
-  //     [color, color, color],
-  //   ];
-}
-
-// 파일을 읽을 때 input 박스에 이벤트를 건다.
-function init() {
-  window.rubiksCube = {
-    up: makeOneSideCube('W'),
-    left: makeOneSideCube('G'),
-    front: makeOneSideCube('R'),
-    right: makeOneSideCube('B'),
-    back: makeOneSideCube('O'),
-    down: makeOneSideCube('Y'),
-  };
-
-  const inputValue = document.querySelector('#inputValue');
-  inputValue.addEventListener('keypress', pressEnter);
-}
-
-init();
-
+// 입력받은 면과 주위 면을 변경
 function turnCube(char) {
   let charUpperCase = char[0].toUpperCase();
   let oneSide = rubiksCube[convertChar(charUpperCase)];
@@ -132,6 +113,7 @@ function turnCube(char) {
   moveSide(char);
 }
 
+// 입력받은 문자를 문자열로 변경
 function convertChar(char) {
   let result;
 
@@ -152,6 +134,7 @@ function convertChar(char) {
   return result;
 }
 
+// 입력받은 면을 시계 방향으로 회전
 function turnClock(side) {
   let copySide = [[], [], []];
 
@@ -164,6 +147,7 @@ function turnClock(side) {
   return copySide;
 }
 
+// 입력받은 면을 반시계 방향으로 회전
 function turnCounterClock(side) {
   let copySide = [[], [], []];
 
@@ -176,6 +160,7 @@ function turnCounterClock(side) {
   return copySide;
 }
 
+// 입력받은 면의 주위 면들을 변경
 function moveSide(char) {
   let charUpperCase = char[0].toUpperCase();
   let copyCube = JSON.parse(JSON.stringify(rubiksCube));
@@ -187,10 +172,10 @@ function moveSide(char) {
   } else if (charUpperCase === 'F' || charUpperCase === 'B') {
     moveFrontBack(char, copyCube);
   }
-
-  consoleCube(rubiksCube);
 }
 
+// up, down
+// up, down 면이 입력 될 시 left, right, front, back 면 변경
 function moveUpDown(char, cube) {
   let idx = char[0].toUpperCase() === 'U' ? 0 : 2;
   let order = ['left', 'front', 'right', 'back'];
@@ -207,6 +192,8 @@ function moveUpDown(char, cube) {
   }
 }
 
+// left, right
+// left, right 면이 입력 될 시 front, back, up, down 면 병경
 function moveLeftRight(char, cube) {
   let idx = [0, 2];
   let order = leftRightOderHelper(char);
@@ -223,6 +210,8 @@ function moveLeftRight(char, cube) {
   }
 }
 
+// left, right
+// left, right 면이 입력 될 시 order를 재조합 해줌
 function leftRightOderHelper(char) {
   let order = ['down', 'front', 'up', 'back'];
   let side;
@@ -239,6 +228,8 @@ function leftRightOderHelper(char) {
   return order;
 }
 
+// left, right
+// left, right 면이 입력 될 시 실질적으로 적용
 function moveLeftRightHelper(index, order, cube, i, j) {
   let idx = index.slice();
   if (i === 0 || i === 1) {
@@ -251,6 +242,8 @@ function moveLeftRightHelper(index, order, cube, i, j) {
   }
 }
 
+// front, back
+// front, back 면이 입력 될 시 up, down, left, right 면 변경
 function moveFrontBack(char, cube) {
   let idx = [0, 2];
   let apostrophe = false;
@@ -270,11 +263,8 @@ function moveFrontBack(char, cube) {
   }
 }
 
-// turnCube('f'); ["up", "right", "down", "left"]
-// turnCube("f'"); ["left", "up", "right", "down"]
-// turnCube('b'); ["right", "down", "left", "up"]
-// turnCube("b'"); ["down", "right", "up", "left"]
-
+// front, back
+// front, back 면이 입력 될 시 order 재조합
 function frontBackOderHelper(char) {
   let order;
 
@@ -291,6 +281,8 @@ function frontBackOderHelper(char) {
   return order;
 }
 
+// front, back
+// front, back 면이 입력 될 시 실질적으로 적용
 function moveFrontBackHelper(index, order, cube, i, j, apostrophe) {
   let idx = index.slice();
 
@@ -313,9 +305,75 @@ function moveFrontBackHelper(index, order, cube, i, j, apostrophe) {
   }
 }
 
+// 만들어진 루빅스 큐브를 웹에 보여줌
+function showResult(char) {
+  let result;
+  if (char === 'Q') {
+    result = '경과시간 조작갯수 감사합니다.';
+  } else {
+    result = char + cubeToString();
+  }
+
+  makeDom('#outputValue', 'li', 'result', result);
+}
+
+// 루빅스 큐브를 웹에 보여주기 위해 문자열로 변경
+function cubeToString() {
+  return `
+    <br> ${blankOutput(10)} ${lineToString('up', 0)} 
+    <br> ${blankOutput(10)} ${lineToString('up', 1)} 
+    <br> ${blankOutput(10)} ${lineToString('up', 2)} 
+    <br> 
+    <br> ${lineToString('left', 0)} &nbsp ${lineToString('front', 0)} &nbsp 
+         ${lineToString('right', 0)} &nbsp ${lineToString('back', 0)} 
+    <br> ${lineToString('left', 1)} &nbsp ${lineToString('front', 1)} &nbsp 
+         ${lineToString('right', 1)} &nbsp ${lineToString('back', 1)} 
+    <br> ${lineToString('left', 2)} &nbsp ${lineToString('front', 2)} &nbsp 
+         ${lineToString('right', 2)} &nbsp ${lineToString('back', 2)} 
+    <br>     
+    <br> ${blankOutput(11)} ${lineToString('down', 0)} 
+    <br> ${blankOutput(11)} ${lineToString('down', 1)} 
+    <br> ${blankOutput(11)} ${lineToString('down', 2)} 
+  `;
+}
+
+// 입력받은 면과 index 값으로 문자를 재조합 후 반환
+function lineToString(side, num) {
+  let result = '';
+  for (let i = 0; i < 3; i++) {
+    result += rubiksCube[side][num][i] + '  ';
+  }
+  return result;
+}
+
+// 입력받은 숫자만큼 공백을 만들어 줌
+function blankOutput(num) {
+  let blank = '';
+  for (let i = 0; i < num; i++) {
+    blank += '&nbsp';
+  }
+  return blank;
+}
+
+// shuffleButton버튼 클릭 시 초기상태 변경
+function initialCube() {
+  const target = document.querySelector('.cube');
+  target.remove();
+  makeDom('#initialCube', 'div', 'cube', cubeToString());
+}
+
+// 타겟태그, 만들태그, 만들클래스이름, 넣을내용 입력 시 첫번째 자식노드로 삽입
+function makeDom(targetId, tag, className, result) {
+  const target = document.querySelector(targetId);
+  const newResult = document.createElement(tag);
+  newResult.classList.add(className);
+  newResult.innerHTML = result;
+  target.insertBefore(newResult, target.firstChild);
+}
+
+init();
 // turnCube('f');
 
-// 숫자에 따른 회전
 // q 입력 시 구현
 // dom 구현
 // 경과 시간 구현
