f88a864ebca2b977caa8bf56fac6b46904fc565d
정인호 <derosatam76@gmail.com>
2020-12-11 20:54:16 +0900

    Refactor split shoveTheWord() function in ShoveTheWord.js file

--- components/ShoveTheWord.js
+++ components/ShoveTheWord.js
@@ -1,49 +1,14 @@
-// dom을 이용한 결과 출력
-function showResult(result) {
-  const outputValue = document.querySelector('#outputValue');
-  const newResult = document.createElement('li');
-  newResult.classList.add('result');
-  newResult.textContent = result;
-  outputValue.insertBefore(newResult, outputValue.firstChild);
+// 파일을 읽을 때 input 박스에 이벤트를 건다.
+function init() {
+  const inputValue = document.querySelector('#inputValue');
+  inputValue.addEventListener('keypress', pressEnter);
 }
 
-// 단어를 밀어내는 함수
-function shoveTheWord(word, int, cmd) {
-  let wordArr = word.split('');
-  cmd = cmd.toUpperCase();
-
-  if (int < 0) {
-    if (cmd === 'L') {
-      cmd = 'R';
-    } else if (cmd === 'R') {
-      cmd = 'L';
-    }
+// enter 클릭 시 입력값에 대한 유효성검사 함수 실행.
+function pressEnter(e) {
+  if (e.keyCode === 13) {
+    validateInputValue(e.target.value);
   }
-
-  if (cmd === 'L') {
-    for (let i = 0; i < Math.abs(int); i++) {
-      let char = wordArr.shift();
-      wordArr.push(char);
-    }
-  } else if (cmd == 'R') {
-    for (let i = 0; i < Math.abs(int); i++) {
-      let char = wordArr.pop();
-      wordArr.unshift(char);
-    }
-  }
-
-  return wordArr.join('');
-}
-
-// 명령에 대한 유효성 검사
-function validateCommand(cmd) {
-  return cmd.toUpperCase() === 'L' || cmd.toUpperCase() === 'R';
-}
-
-// 숫자에 대한 유효성 검사
-function validateInteger(integer) {
-  const int = Number(integer);
-  return -100 <= int && int < 100 && Number.isInteger(int);
 }
 
 // 입력값에 대한 유효성 검사
@@ -64,17 +29,59 @@ function validateInputValue(value) {
   }
 }
 
-// enter 클릭 시 입력값에 대한 유효성검사 함수 실행.
-function pressEnter(e) {
-  if (e.keyCode === 13) {
-    validateInputValue(e.target.value);
-  }
+// 숫자에 대한 유효성 검사
+function validateInteger(integer) {
+  const int = Number(integer);
+  return -100 <= int && int < 100 && Number.isInteger(int);
 }
 
-// 파일을 읽을 때 input 박스에 이벤트를 건다.
-function init() {
-  const inputValue = document.querySelector('#inputValue');
-  inputValue.addEventListener('keypress', pressEnter);
+// 명령에 대한 유효성 검사
+function validateCommand(cmd) {
+  return cmd.toUpperCase() === 'L' || cmd.toUpperCase() === 'R';
+}
+
+// 단어를 밀어내는 함수
+function shoveTheWord(word, int, cmd) {
+  let wordArr = word.split('');
+  cmd = checkCommand(cmd, int);
+
+  if (cmd === 'L') {
+    for (let i = 0; i < Math.abs(int); i++) {
+      let char = wordArr.shift();
+      wordArr.push(char);
+    }
+  } else if (cmd == 'R') {
+    for (let i = 0; i < Math.abs(int); i++) {
+      let char = wordArr.pop();
+      wordArr.unshift(char);
+    }
+  }
+
+  return wordArr.join('');
+}
+
+// 음수인지 양수인지를 확인 후 cmd 반환
+function checkCommand(cmd, int) {
+  cmd = cmd.toUpperCase();
+
+  if (int < 0) {
+    if (cmd === 'L') {
+      cmd = 'R';
+    } else if (cmd === 'R') {
+      cmd = 'L';
+    }
+  }
+
+  return cmd;
+}
+
+// dom을 이용한 결과 출력
+function showResult(result) {
+  const outputValue = document.querySelector('#outputValue');
+  const newResult = document.createElement('li');
+  newResult.classList.add('result');
+  newResult.textContent = result;
+  outputValue.insertBefore(newResult, outputValue.firstChild);
 }
 
 init();
